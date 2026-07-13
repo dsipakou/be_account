241fa368ed7edeed18525f882f7d8330c02dd98b
Denis Sipakov <mc.flyer@gmail.com>
2026-07-13 11:55:00 +0200

    Capital expense category added

--- account/apps/categories/constants.py
+++ account/apps/categories/constants.py
@@ -1,12 +1,15 @@
 INCOME = "INC"
 EXPENSE = "EXP"
+CAPITAL_EXPENSE = "CAP"
 
 CATEGORY_TYPES = (
     (INCOME, "Income"),
     (EXPENSE, "Expense"),
+    (CAPITAL_EXPENSE, "Capital Expense"),
 )
 
 CategoryType = (
     INCOME,
     EXPENSE,
+    CAPITAL_EXPENSE,
 )
